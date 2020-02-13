17e4c58a1611815f4db978bb110c29673cdee545
ken <kentaro.w0918@gmail.com>
2020-02-13 19:10:28 +0900

    2/13

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/0213', (req, res) => res.render('articles/0213news'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
